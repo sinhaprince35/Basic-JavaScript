5d6a897671ca57ce4ead733006bccf88b4d42916
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2022-12-29 19:23:37 +0530

    third commit

---
+++ Input-Output-in-JS.js
@@ -0,0 +1,6 @@
+const p = Number(prompt("Enter First Number"));     // Prompt is used to input number
+const s = Number(prompt("Enter Second Number"));
+sum = p + s;
+console.log(sum);
+alert(sum);                                         // Alert shows the output.
+document.write(sum);                                // document.write() shows output in the browser not in the console
